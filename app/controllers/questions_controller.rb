724cd5c9b10714829b3592e3c3267be1d4ef255b
Rokas Stasiulis <rokastas@gmail.com>
2024-02-06 23:29:36 +0100

    bug fixes in the chatGPT promt and response

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,38 +2,43 @@ class QuestionsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def show
+    prompt = "solar system"
+
+    # prompt = "The Solar System is made up of all the planets that orbit our Sun. In addition to planets, the Solar System also consists of moons, comets, asteroids, minor planets, dust and gas. The inner solar system contains the Sun, Mercury, Venus, Earth and Mars. The main asteroid belt lies between the orbits of Mars and Jupiter. The planets of the outer solar system are Jupiter, Saturn, Uranus and Neptune (Pluto is now classified as a dwarf planet)."
+
     client = OpenAI::Client.new
     chatgpt_response = client.chat(parameters: {
       model: "gpt-3.5-turbo-0125",
       messages: [{
         role: "system",
-        content: "You are a helpful assistant that generate quiz questions based on the provided content. Respond with five short questions and four plausible options/ answers for each question, of which only one is correct. Provide your answer in a  JSON structure similar to this, do not use single or double quotes inside questions and answers
+        content: "You are a helpful assistant that generates quiz questions based on the provided content. Respond with five short questions and four plausible options/ answers for each question, of which only one is correct. Provide your answer in a  JSON structure similar to this.
           [
             {
               'topic: '<The topic of the quiz>',
               'question': '<The quiz question you generate>',
               'options': {
-                'option1': {'body': 'Plausible option 1>', 'isItCorrect': ‹true or false>},
-                'option2': {'body': '<Plausible option 2>', 'isItCorrect': ‹true or false>},
-                'option3': {'body': '«Plausible option 3>', 'isItCorrect': ‹true or false>},
-                'option4': {'body': '«Plausible option 4>', 'isItCorrect': ‹true or false>}
+                'option1': {'body': '<Plausible option 1>', 'isItCorrect': <true or false>},
+                'option2': {'body': '<Plausible option 2>', 'isItCorrect': <true or false>},
+                'option3': {'body': '<Plausible option 3>', 'isItCorrect': <true or false>},
+                'option4': {'body': '<Plausible option 4>', 'isItCorrect': <true or false>}
               }
             },
             {
               'topic: '<The topic of the quiz>',
               'question': '<The quiz question you generate>',
               'options': {
-                'option1': {'body': 'Plausible option 1>', 'isItCorrect': ‹true or false>},
-                'option2': {'body': '<Plausible option 2>', 'isItCorrect': ‹true or false>},
-                'option3': {'body': '«Plausible option 3>', 'isItCorrect': ‹true or false>},
-                'option4': {'body': '«Plausible option 4>', 'isItCorrect': ‹true or false>}
+                'option1': {'body': '<Plausible option 1>', 'isItCorrect': <true or false>},
+                'option2': {'body': '<Plausible option 2>', 'isItCorrect': <true or false>},
+                'option3': {'body': '<Plausible option 3>', 'isItCorrect': <true or false>},
+                'option4': {'body': '<Plausible option 4>', 'isItCorrect': <true or false>}
               }
             }
-          ]"
+          ]
+          Under no circumstances use double quotes in your JSON response. Use single quotes instead. If you use double quotes, the JSON will be invalid and an error will occur. If you are unsure about the JSON structure, please refer to the example above."
       },
       {
         role: "user",
-        content: "Generate a multiple choice quiz about the solar system. Give me only the text of the quiz, without any of your own answer like 'Here is a quiz I made'."
+        content: "Generate a multiple choice quiz about: #{prompt}. Give me only the text of the quiz, without any of your own answer like 'Here is a quiz I made'."
       },
       {
         role: "assistant",
@@ -41,36 +46,9 @@ class QuestionsController < ApplicationController
         'options': {'option1': {'body': 'France', 'isItCorrect': false}, 'option2': {'body': 'England', 'isItCorrect': true}, 'option3': {'body': 'Sweden', 'isItCorrect': false}}}"
       }]
     })
-    # parsed_response = JSON.parse(chatgpt_response["choices"][0]["message"]["content"])
-    # @content = parsed_response['question']
-    raw_content = chatgpt_response["choices"][0]["message"]["content"].gsub("'", '"')
+
+    # @content = chatgpt_response["choices"][0]["message"]["content"]
+    raw_content = chatgpt_response["choices"][0]["message"]["content"].gsub("\\'", "AAA").gsub('"', '\\"').gsub("'", '"').gsub("AAA", "'")
     @content = JSON.parse(raw_content)
-    # @question = @content["question"]
   end
 end
-
-      # console test
-
-# client = OpenAI::Client.new
-# client.chat(parameters: {
-#   model: "gpt-3.5-turbo-0125",
-#   messages: [{
-#     role: "system",
-#     content: "You are a helpful assistant that generate quiz questions based on the provided content. Respond with five short questions and four plausible options/ answers for each question, of which only one is correct. Provide your answer in JSON structure like this {'topic: '<The topic of the quiz>', 'question': '<The quiz question you generate>', 'options': { 'option1': {'body': 'Plausible option 1>', 'isItCorrect': ‹true or false>}, 'option2': {'body': '<Plausible option 2>', 'isItCorrect': ‹true or false>}, 'option3': {'body': '«Plausible option 3>', 'isItCorrect': ‹true or false>}}}"
-#   },
-#   {
-#     role: "user",
-#     content: "Generate a multiple choice quiz about the solar system. Give me only the text of the quiz, without any of your own answer like 'Here is a quiz I made'."
-#   },
-#   {
-#     role: "assistant",
-#     content: "{'topic': 'Premier League location', 'question': 'Where is the Premier League played?',
-#     'options': {'option1': {'body': 'France', 'isItCorrect': false}, 'option2': {'body': 'England', 'isItCorrect': true}, 'option3': {'body': 'Sweden', 'isItCorrect': false}}}"
-#   }]
-# })
-
-
-
-
-
-# "You are a helpful assistant that generate quiz questions based on the provided content. Respond with five short questions and four plausible options/ answers for each question, of which only one is correct. Provide your answer in a  JSON structure similar to this {'topic: '<The topic of the quiz>', 'question': '<The quiz question you generate>', 'options': { 'option1': {'body': 'Plausible option 1>', 'isItCorrect': ‹true or false>}, 'option2': {'body': '<Plausible option 2>', 'isItCorrect': ‹true or false>}, 'option3': {'body': '«Plausible option 3>', 'isItCorrect': ‹true or false>}}}"
