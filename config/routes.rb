530b55d20a42711211c61c9e62a2e8a9bb530bb4
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-02-06 14:03:18 +0000

    log in

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "pages#home"
+  # root "pages#home"
 
   resources :quizzes, only: [ :index, :show, :new, :create, :edit ] do
     resources :questions, only: [ :create ]
